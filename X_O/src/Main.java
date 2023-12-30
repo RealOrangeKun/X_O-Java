b8bcf47343f21327db0338e42acf57814f6f6c85
Orangekun <amazingritro66@gmail.com>
2023-12-30 15:28:05 +0200

    added .exe file

--- X_O/src/Main.java
+++ X_O/src/Main.java
@@ -20,7 +20,7 @@ public class Main {
             System.out.println("User canceled");
             System.exit(0);
         }
-        MainMenu mainMenu = new MainMenu();
+        new MainMenu();
 
     }
 }
