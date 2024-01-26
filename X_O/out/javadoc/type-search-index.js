0818b0d8c91f2f10212b643585f10e99d6d86bff
Orangekun <amazingritro66@gmail.com>
2024-01-26 14:34:26 +0200

    Moved javadoc folder

--- X_O/out/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"AIPlayer"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Controller"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"MainMenu"},{"p":"<Unnamed>","l":"MusicPlayer"},{"p":"<Unnamed>","l":"SettingsMenu"},{"p":"<Unnamed>","l":"XOBoard"}];updateSearchResults();
